bc6a8fc7eb8b1ab4183eceefecb2c12f7c4d03cd
duggank <CreativeWeep@gmail.com>
2014-09-16 08:44:50 -0400

    Initial Commit
    Added my Project

---
+++ Chapter4/Chapter4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int user;
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.Write("Enter a number from 1 - 998: ");
+                input = Console.ReadLine();
+                user = Convert.ToInt32(input);
+                if (user == 999 || user > 999)
+                    break;
+            }
+        }
+    }
+}
---
+++ Chapter4/GetVowel.cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GetVowel.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 1000; i++)
+            {
+
+        }
+    }
+}
